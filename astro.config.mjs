cab8e1357055094ab23c1da5da2806b3aa10194b
sandovaldavid <sandovaldavid2201@gmail.com>
2025-05-09 15:24:30 -0500

    🔧 Configure Astro with integrations for icon, MDX, sitemap, and Alpine.js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from 'astro-icon';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [icon(), mdx(), sitemap(), alpinejs()],
+});
