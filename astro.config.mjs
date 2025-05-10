0175b876dd0ed9312391b21b63b39d9ffebbeee1
sandovaldavid <sandovaldavid2201@gmail.com>
2025-05-09 20:17:08 -0500

    🎨 format code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import icon from 'astro-icon';
-
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
-
 import alpinejs from '@astrojs/alpinejs';
-
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [icon(), mdx(), sitemap(), alpinejs()],
-    vite: {
-    plugins: [tailwindcss()],
-  },
+	integrations: [icon(), mdx(), sitemap(), alpinejs()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
